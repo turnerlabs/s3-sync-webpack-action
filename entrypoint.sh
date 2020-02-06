b8d4d5d351418791a57965badd7ce4be6c7d3589
Michael Smith <smithatlanta@gmail.com>
2020-02-06 18:20:41 -0500

    lets see if this works

---
+++ entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$AWS_S3_BUCKET" ]; then
+  echo "AWS_S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_DEFAULT_REGION" ]; then
+  echo "AWS_DEFAULT_REGION is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$SOURCE_DIR" ]; then
+  echo "SOURCE_DIR is not set. Quitting."
+  exit 1
+fi
+
+
+mkdir -p ~/.aws
+touch ~/.aws/credentials
+
+cd $SOURCE_DIR
+
+npx webpack --mode development
+
+echo "[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+ 
+if [ -d "$SOURCE_DIR" ]; then
+    echo "Copying to website folder"
+    aws s3 sync ${SOURCE_DIR}/sdks/javascript/dist/ s3://${AWS_S3_BUCKET} --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
+fi
+
+echo "Cleaning up things"
+
+rm -rf ~/.aws
